e14baa599166252ee7e5ca8a244859c8325f517b
Gabr19 <sannagabriele19@gmail.com>
2023-03-14 13:17:57 +0100

    .

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,3 +1,11 @@
 <header>
-    header
+    <div class="container container-header">
+        <div class="row">
+            <div class="col card">
+                <div class="img">
+                    <img src="../public/images/jumbotron" alt="">
+                </div>
+            </div>            
+        </div>
+    </div>
 </header>
